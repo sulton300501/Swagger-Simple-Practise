00f0552220d3a86cc494e1f8ecb33c0a3234b6a8
sulton <sultonmalik326@gmail.com>
2024-03-26 18:04:12 +0500

    Add project files.

---
+++ DotNetAuthentication/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNetAuthentication.Data
+{
+    public class DataContext:IdentityDbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+            
+        }
+
+    }
+}
---
+++ DotNetAuthentication/Program.cs
@@ -0,0 +1,65 @@
+
+using DotNetAuthentication.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace DotNetAuthentication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    In=ParameterLocation.Header,
+                    Name="Authorization",
+                    Type=SecuritySchemeType.ApiKey
+                });
+                options.OperationFilter<SecurityRequirementsOperationFilter>();
+            });
+
+
+            builder.Services.AddDbContext<DataContext>(option =>
+            {
+                option.UseNpgsql(builder.Configuration.GetConnectionString("FutureProjectsConnectionString"));
+            });
+
+            builder.Services.AddAuthorization();
+            builder.Services.AddIdentityApiEndpoints<IdentityUser>()
+                .AddEntityFrameworkStores<DataContext>();
+
+
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.MapIdentityApi<IdentityUser>();
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
